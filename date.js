175d3b0f3ff006ba770ce23873379093aa6e0077
silviansi <silviansi24@gmail.com>
2023-03-23 10:35:34 +0700

    initial commit

---
+++ date.js
@@ -0,0 +1,16 @@
+function dateNow() {
+    array_bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", 
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+    date = new Date();
+    tanggal = date.getDate();
+    bulan = date.getMonth();
+    tahun = date.getFullYear();
+    jam = date.getHours();
+    menit = date.getMinutes();
+    detik = date.getSeconds();
+
+    const tampil = tanggal + " " + array_bulan[bulan] + " " + tahun + " " + jam + ":" + menit + ":" + detik
+    console.log(tampil);
+}
+
+dateNow()
---
+++ fruit.js
@@ -0,0 +1,20 @@
+//Membuat variable dengan let dan const
+const buah = "Anggur";
+let jumlah = 5;
+
+//Menampilkan isi variable
+console.log(buah, jumlah);
+
+//Membuat array berisi 5 buah-buahan
+const fruits = ["Apel", "Belimbing", "Chery", "Durian", "Jambu"];
+//Menambah satu buah
+fruits.push("Melon");
+console.log(fruits);
+
+//Mengubah satu buah
+fruits[2] = "Semangka";
+console.log(fruits);
+
+//Menghapus array terakhir
+fruits.pop()
+console.log(fruits);
